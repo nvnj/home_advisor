35524a2fceb298b8e0bd1cd2705e077c5d0ad939
jisjoezartek <techstack2091@gmail.com>
2020-12-12 03:47:43 +0530

    save state

---
+++ lib/ui/otp_page/login_view.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:home_advisor/app_theme/app_colors.dart';
+import 'package:home_advisor/app_theme/screen_util-extension.dart';
+import 'package:home_advisor/app_theme/text_styles.dart';
+import 'package:home_advisor/ui/otp_page/login_view_model.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
+import 'package:stacked/stacked.dart';
+
+class LoginView extends StatelessWidget {
+  static const String id = "LoginView";
+
+  const LoginView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<LoginViewModel>.reactive(
+      onModelReady: (model)=>model.initState(),
+      builder: (context, model, child) => Scaffold(
+        body: _Body(),
+      ),
+      viewModelBuilder: () => LoginViewModel(),
+    );
+  }
+}
+
+class _Body extends ViewModelWidget<LoginViewModel> {
+  @override
+  Widget build(BuildContext context, LoginViewModel model) {
+    return SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(20.w),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              model.getHeaderText(),
+              style: AppTextStyles.s3(
+                AppColor.blCommon,
+              ).copyWith(fontSize: 30),
+            ),
+            SizedBox(height: 100.w),
+            model.showOtp() == true ? _OtpInput() : _MobileInput(),
+            SizedBox(height: 100.w),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  margin: EdgeInsets.only(left: 10, right: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                    gradient: LinearGradient(
+                      colors: [
+                        AppColor.blGradient2,
+                        AppColor.blGradient1,
+                      ],
+                    ),
+                  ),
+                  height: 100.h,
+                  child: FlatButton(
+                    onPressed: () {
+                      if (model.isMobileNoValid) model.validatePhone();
+                    },
+                    child: Text(
+                      model.getButtonText(),
+                    ),
+                    textColor: Colors.white,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _OtpInput extends ViewModelWidget<LoginViewModel> {
+  @override
+  Widget build(BuildContext context, LoginViewModel model) {
+    return PinCodeTextField(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      length: 6,
+      animationType: AnimationType.fade,
+      pinTheme: PinTheme(
+        shape: PinCodeFieldShape.underline,
+        inactiveColor: Theme.of(context).focusColor,
+        activeColor: Theme.of(context).focusColor,
+        activeFillColor: Colors.transparent,
+        inactiveFillColor: Colors.transparent,
+        selectedFillColor: Colors.transparent,
+        selectedColor: Theme.of(context).focusColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      animationDuration: Duration(milliseconds: 300),
+      backgroundColor: Colors.transparent,
+      enableActiveFill: true,
+      onCompleted: (otp) {
+        print("Completed");
+        model.validateOtp(otp);
+      },
+      onChanged: (value) {
+        print(value);
+      },
+      beforeTextPaste: (text) {
+        print("Allowing to paste $text");
+        //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+        //but you can show anything you want here, like your pop up saying wrong paste format or etc
+        return true;
+      },
+      appContext: context,
+    );
+  }
+}
+
+class _MobileInput extends ViewModelWidget<LoginViewModel> {
+  _MobileInput({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, LoginViewModel model) {
+    return Container(
+      color: Colors.transparent,
+      child: InternationalPhoneNumberInput(
+        initialValue: model.getDefaultCountry(),
+
+        onInputChanged: (PhoneNumber number) {
+          model.phoneNumber = number.phoneNumber.substring(3);
+        },
+        // initialValue: number,
+
+        // autoValidate: true,
+        onInputValidated: (bool value) {
+          model.isMobileNoValid = value;
+        },
+        inputDecoration: new InputDecoration(
+          fillColor: Colors.transparent,
+          filled: true,
+        ),
+        ignoreBlank: true,
+        // initialValue: model.initialMobileValue,
+        selectorTextStyle: TextStyle(color: Colors.black),
+        // inputBorder: OutlineInputBorder(),
+        selectorConfig:
+            SelectorConfig(selectorType: PhoneInputSelectorType.BOTTOM_SHEET),
+        countrySelectorScrollControlled: true,
+      ),
+    );
+  }
+}
---
+++ lib/ui/otp_page/login_view_model.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:home_advisor/app/locator.dart';
+import 'package:home_advisor/services/auth/auth_service.dart';
+import 'package:home_advisor/services/auth/firebase_auth_service.dart';
+import 'package:home_advisor/ui/homepage/home_page_view.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:stacked/stacked.dart';
+
+class LoginViewModel extends BaseViewModel {
+  AuthService _authService = locator<FirebaseAuthService>();
+
+  PhoneAuthenticationSteps _currentStep = PhoneAuthenticationSteps.INIT;
+
+  PhoneAuthenticationSteps get currentStep => _currentStep;
+
+  set currentStep(PhoneAuthenticationSteps currentStep) {
+    _currentStep = currentStep;
+    switch (_currentStep) {
+      case PhoneAuthenticationSteps.AUTHENTICATION_SUCCESS:
+        setBusy(false);
+        break;
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED_NETWWORK:
+      case PhoneAuthenticationSteps.AUTO_RETRIVAL_TIMEOUT:
+      case PhoneAuthenticationSteps.INVALID_OTP_ENTERED:
+      case PhoneAuthenticationSteps.INIT:
+        setBusy(false);
+        break;
+      case PhoneAuthenticationSteps.AUTO_RETRIEVING_CODE:
+      case PhoneAuthenticationSteps.CODE_SENT:
+        setBusy(false);
+        break;
+    }
+    notifyListeners();
+  }
+
+  initState() {
+    _authService.onPhoneAuthenticationStepChanged.listen((event) {
+      if (currentStep == PhoneAuthenticationSteps.AUTHENTICATION_SUCCESS)
+        return;
+      currentStep = event;
+    });
+  }
+
+  String _phoneNumber;
+
+  bool _isOtpRequested = false;
+
+  bool get isOtpRequested => _isOtpRequested;
+
+  set isOtpRequested(bool isOtpRequested) {
+    _isOtpRequested = isOtpRequested;
+    notifyListeners();
+  }
+
+  String get phoneNumber => _phoneNumber;
+
+  set phoneNumber(String initialMobileValue) {
+    _phoneNumber = initialMobileValue;
+    notifyListeners();
+  }
+
+  bool _isMobileNoValid = false;
+
+  bool get isMobileNoValid => _isMobileNoValid;
+
+  set isMobileNoValid(bool isMobileNoValid) {
+    _isMobileNoValid = isMobileNoValid;
+    notifyListeners();
+  }
+
+  void validatePhone() {
+    setBusy(true);
+    _authService.signInWithPhone(phoneNumber);
+  }
+
+  void validateOtp(String otp) {
+    print(otp);
+    _authService.validatePhoneOtp(otp);
+  }
+
+  String getHeaderText() {
+    switch (currentStep) {
+      case PhoneAuthenticationSteps.INIT:
+      case PhoneAuthenticationSteps.AUTO_RETRIVAL_TIMEOUT:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED_NETWWORK:
+      case PhoneAuthenticationSteps.AUTO_RETRIEVING_CODE:
+        return "Welcome Back\nHome Advisor";
+      case PhoneAuthenticationSteps.INVALID_OTP_ENTERED:
+      case PhoneAuthenticationSteps.CODE_SENT:
+      case PhoneAuthenticationSteps.AUTHENTICATION_SUCCESS:
+        return "Enter OTP Recevied";
+    }
+    return "Welcome Back\nHome Advisor";
+  }
+
+  String getButtonText() {
+    switch (currentStep) {
+      case PhoneAuthenticationSteps.INIT:
+      case PhoneAuthenticationSteps.AUTO_RETRIVAL_TIMEOUT:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED_NETWWORK:
+      case PhoneAuthenticationSteps.AUTO_RETRIEVING_CODE:
+        return "SUBMIT";
+      case PhoneAuthenticationSteps.INVALID_OTP_ENTERED:
+      case PhoneAuthenticationSteps.CODE_SENT:
+      case PhoneAuthenticationSteps.AUTHENTICATION_SUCCESS:
+        return "CONFIRM";
+    }
+    return "SUBMIT";
+  }
+
+  bool showOtp() {
+    switch (currentStep) {
+      case PhoneAuthenticationSteps.INIT:
+      case PhoneAuthenticationSteps.AUTO_RETRIVAL_TIMEOUT:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED:
+      case PhoneAuthenticationSteps.AUTHENTICATION_FAILED_NETWWORK:
+      case PhoneAuthenticationSteps.AUTO_RETRIEVING_CODE:
+        return false;
+      case PhoneAuthenticationSteps.INVALID_OTP_ENTERED:
+      case PhoneAuthenticationSteps.CODE_SENT:
+      case PhoneAuthenticationSteps.AUTHENTICATION_SUCCESS:
+        return true;
+    }
+    return false;
+  }
+
+  Future<void> buttonAction(BuildContext context) async {
+    if (currentStep == PhoneAuthenticationSteps.CODE_SENT ||
+        currentStep == PhoneAuthenticationSteps.INIT) {
+      validatePhone();
+    } else if (currentStep == PhoneAuthenticationSteps.AUTHENTICATION_FAILED ||
+        currentStep ==
+            PhoneAuthenticationSteps.AUTHENTICATION_FAILED_NETWWORK) {
+      validatePhone();
+    } else if (currentStep == PhoneAuthenticationSteps.AUTHENTICATION_SUCCESS) {
+      authSucessAction(context);
+    }
+    return true;
+  }
+
+  void authSucessAction(BuildContext context) {
+    Navigator.pushNamedAndRemoveUntil(
+        context, HomePageView.id, (route) => false);
+  }
+
+  PhoneNumber getDefaultCountry() {
+    return PhoneNumber(isoCode: "IN");
+  }
+}
