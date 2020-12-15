c484d7c944bcdcd6042744d33f85f7d86f49dfb4
nidhin579 <nidhinvarghese579@gmail.com>
2020-12-15 22:21:12 +0530

    API Changed

--- lib/ui/main_category/main_categ_viewmodel.dart
+++ lib/ui/main_category/main_categ_viewmodel.dart
@@ -10,7 +10,6 @@ class MainCategViewModel extends BaseViewModel {
     var response = await _authService.firebaseAuth.currentUser.getIdToken();
     if (response != null) {
       token = response;
-      print(token);
       notifyListeners();
     }
   }
